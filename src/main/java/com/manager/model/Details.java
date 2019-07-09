3c9d82d2429efc0fbb6f270e40b6ace77142dc9f
VuVanHoc <hocvanvu1999@gmail.com>
2019-07-09 09:44:31 +0700

    add file Details to branch master

---
+++ src/main/java/com/manager/model/Details.java
@@ -0,0 +1,14 @@
+package com.manager.model;
+
+public class Details {
+	public static final String[] positions = new String[]{"Manager", "Admin", "Developer", "Data Analyst",
+			"Accountant", "Fresher Java", "BA", "Sale IT", "Tester"};
+
+	public static final String[] status = {"Active", "Block"};
+
+	public static final String[] roles = {"ROLE_MANAGER", "ROLE_ADMIN", "ROLE_EMPLOYEE"};
+
+	public static final String [] departments = {""};
+
+
+}
